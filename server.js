94eee523dade7c363acc39b560337719a1e45368
davidagitlen <davidagitlen@gmail.com>
2019-10-07 11:33:16 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+// app.use(cors());
+
+app.set('port', process.env.PORT || 3001);
+
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   optionsSuccessStatus: 200
+// }
+
+const snacks = [
+  { snackName: 'tacos', delicious: 'yup', cheap: 'Probably' }, 
+  { snackName: 'okonomiyaki', delicious: 'yes\'m', cheap: 'Depends on fanciness' }, 
+  { snackName: 'sad porridge', delicious: 'probably not', cheap: 'Hell yes' }
+]
+
+app.get('/api/v1/snacks', cors(), (request, response) => {
+  return response.status(200).json({snacks})
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Running on http://localhost:${app.get('port')}.`)
+});
